7cf4663838374f185c060644947bddbfaba17d40
VladislavHub <darkdevil9592@gmail.com>
2024-06-12 22:21:10 +0300

    update

--- src/component/main/index.js
+++ src/component/main/index.js
@@ -6,21 +6,32 @@ import logo from "./svg/coin.svg"
 export default function Page() {
   const initialTotalBalance = localStorage.getItem('totalBalance') || 1000;
   const [totalBalance, setTotalBalance] = useState(parseInt(initialTotalBalance, 10));
+  const [isAnimating, setIsAnimating] = useState(false)
 
   useEffect(() => {
-    // При зміні значення totalBalance оновлюємо його в localStorage
     localStorage.setItem('totalBalance', totalBalance);
   }, [totalBalance]);
 
   const handleClick = () => {
     setTotalBalance(totalBalance + 1);
+    setIsAnimating(true)
   } 
+
+  useEffect(() => {
+    if(isAnimating) {
+      const timer = setTimeout(() => {
+        setIsAnimating(false)
+      }, 100)
+
+      return () => clearTimeout(timer)
+    }
+  }, [isAnimating])
     return (
       <header className="page mine_page">
 
         <div className="total_balance">{totalBalance}</div>
 
-        <div className="main_coin scale" onClick={handleClick}>
+        <div className={`main_coin ${isAnimating ? 'scale' : ''}`} onClick={handleClick}>
           <img src={logo} className="App-logo" alt="logo" />
         </div>
 
